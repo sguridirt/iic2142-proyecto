c23fc6096ab5df919ffb09a0279b494fd0d06b8e
cnselman <cnselman@cselman>
2024-09-05 16:42:16 -0400

    Add project files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your root route ("/")
   # root "home#index"  # Si tienes un controlador llamado HomeController y una acción index
   root "welcome#index"  # Asegúrate de tener un controlador llamado WelcomeController con una acción index
