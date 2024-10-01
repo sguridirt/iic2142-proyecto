06941916d2eeb5cbdfa04394b068238b7ea0bdfa
root <root@LAPTOP-LCS8PR3T.localdomain>
2024-10-01 02:33:27 -0300

    With this featurepuedes ver las solicitudes si eres alumno

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root "pages#index"
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   resources :courses do
     member do
       post 'request_join'  # Esta ruta permite enviar una solicitud para unirse a un curso
     end
   end
+
+  # Añade recursos para las solicitudes
+  resources :requests, only: [:index, :create, :destroy] # Puedes añadir otras acciones según sea necesario
 end
+
 
 
 
