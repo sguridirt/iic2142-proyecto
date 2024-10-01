9fc2f7b1e2fd52b97df50e9e75991f498a7490bb
root <root@LAPTOP-LCS8PR3T.localdomain>
2024-10-01 01:54:06 -0300

    Desarrollo de la pagina de busqueda de cursos

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+    belongs_to :teacher, foreign_key: 'teachers_id'  # Cambia esto si es necesario
+    belongs_to :class_type
 end
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root "pages#index"
   devise_for :users, :controllers => { registrations: 'users/registrations' }
+  resources :courses do
+    member do
+      post 'request_join'  # Esta ruta permite enviar una solicitud para unirse a un curso
+    end
+  end
 end
+
+
+
+
