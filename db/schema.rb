f942149b2d8f5f41340ea57656c16f19be03764e
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-10-01 09:51:27 -0300

    reorder schema user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2024_09_30_220604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -186,16 +185,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_220604) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "phone", default: "", null: false
-    
     t.string "profilePictureURL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_role_id", null: false
-
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "phone", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
