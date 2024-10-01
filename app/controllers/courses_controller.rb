52fe7f99c0535dbff51e1c9ad9b2afb92d8baa9b
root <root@LAPTOP-LCS8PR3T.localdomain>
2024-10-01 01:54:06 -0300

    Desarrollo de la pagina de busqueda de cursos

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,12 @@
+class CoursesController < ApplicationController
+    def index
+      if params[:query].present?
+        @courses = Course.joins(:teacher)
+                         .where("courses.title ILIKE ? OR teachers.user_id IN (SELECT id FROM users WHERE name ILIKE ?)", "%#{params[:query]}%", "%#{params[:query]}%")
+      else
+        @courses = Course.all
+      end
+    end
+end
+
+  
--- app/models/class_request.rb
+++ app/models/class_request.rb
@@ -1,2 +1,7 @@
 class ClassRequest < ApplicationRecord
+    belongs_to :student  # Asegúrate de que este nombre coincida con el modelo Student
+    belongs_to :course   # Asegúrate de que este nombre coincida con el modelo Course
+
+    # Validaciones
+    validates :description, presence: true
 end
