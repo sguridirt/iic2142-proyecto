8fe1b75168bd9429e8ab5c5e9c081760121460e7
cnselman <cnselman@uc.cl>
2024-10-01 22:07:09 -0300

    fix schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_10_01_202441) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +74,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_01_202441) do
     t.string "title", null: false
     t.string "comment", null: false
     t.integer "rating", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "class_types", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
