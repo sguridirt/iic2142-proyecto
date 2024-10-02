aac94f45c690f26915c73b2b22c0ab1938fed122
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-10-02 09:39:31 -0300

    fix Course model association

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
-    belongs_to :teacher, foreign_key: 'teacher_id'  # Cambia esto si es necesario
-    belongs_to :class_type
+  belongs_to :teacher
+  belongs_to :course_type
 end
 
   
--- app/models/course_type.rb
+++ app/models/course_type.rb
@@ -1,3 +1,3 @@
 class CourseType < ApplicationRecord
-    has_many :courses
+  has_many :courses
 end
