7a13cde2047a8eaed543e3eff142a1700e53b1b6
cnselman <cnselman@uc.cl>
2024-10-04 10:46:06 -0300

    Fix Rubocop

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,8 @@ class CoursesController < ApplicationController
   def index
     if params[:query].present?
       @courses = Course.joins(:teacher)
-                       .where("courses.title ILIKE ? OR teachers.user_id IN (SELECT id FROM users WHERE name ILIKE ?)", "%#{params[:query]}%", "%#{params[:query]}%")
+                       .where("courses.title ILIKE ? OR teachers.user_id IN (SELECT id FROM users WHERE name ILIKE ?)",
+                              "%#{params[:query]}%", "%#{params[:query]}%")
     else
       @courses = Course.all
     end
