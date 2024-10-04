050163659eb28c81ee2f02fe73f2a788702fc909
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-10-04 19:12:23 -0300

    fix migrations with foreign keys

--- db/migrate/20241003133726_add_course_id_to_materials.rb
+++ db/migrate/20241003133726_add_course_id_to_materials.rb
@@ -1,7 +1,5 @@
 class AddCourseIdToMaterials < ActiveRecord::Migration[7.0]
-  # def change
-  #   add_column :materials, :course_id, :integer
-  #   add_foreign_key :materials, :courses, column: :course_id
-  #   add_index :materials, :course_id
-  # end
+  def change
+    add_reference :materials, :course, null: false, foreign_key: true, on_delete: :cascade
+  end
 end
