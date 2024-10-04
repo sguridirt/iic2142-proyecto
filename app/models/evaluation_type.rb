6e92689275caf18dd8e17a81368542ae86f32ca4
cnselman <cnselman@uc.cl>
2024-10-04 10:46:06 -0300

    Fix Rubocop

--- app/models/evaluation_type.rb
+++ app/models/evaluation_type.rb
@@ -1,5 +1,5 @@
 class EvaluationType < ApplicationRecord
-    has_many :evaluations, dependent: :restrict_with_exception
-    validates :name, presence: true
-    validates :description, presence: true
+  has_many :evaluations, dependent: :restrict_with_exception
+  validates :name, presence: true
+  validates :description, presence: true
   end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,9 +1,9 @@
 class Material < ApplicationRecord
-    belongs_to :course, foreign_key: :course_id
-    belongs_to :material_type 
+  belongs_to :course, foreign_key: :course_id
+  belongs_to :material_type 
 
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :material_type, presence: true
-    validates :course_id, presence: true 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :material_type, presence: true
+  validates :course_id, presence: true 
 end
