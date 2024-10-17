490febd42d5b61cdc371e66976d09201e16b2b9b
sguridrt <34144826+sguridirt@users.noreply.github.com>
2024-10-17 13:00:22 -0300

    fix indentation

--- app/controllers/evaluation_answers_controller.rb
+++ app/controllers/evaluation_answers_controller.rb
@@ -1,24 +1,24 @@
 class EvaluationAnswersController < ApplicationController
-    before_action :authenticate_user!
-  
-    def create
-      evaluation = Evaluation.find(params[:evaluation_id])
-      course = evaluation.course
-  
-      params[:answers].each do |question_id, answer_data|
-        content = answer_data[:content].presence || "no se respondió"
-  
-        EvaluationAnswer.create(
-          content: content,
-          points: 0,
-          evaluation_status: 1, 
-          evaluation_question_id: answer_data[:evaluation_question_id],
-          student_id: answer_data[:student_id]
-        )
-      end
-  
-      flash[:notice] = "Respuestas guardadas correctamente."
-      redirect_to course_path(course)
+  before_action :authenticate_user!
+
+  def create
+    evaluation = Evaluation.find(params[:evaluation_id])
+    course = evaluation.course
+
+    params[:answers].each do |question_id, answer_data|
+      content = answer_data[:content].presence || "no se respondió"
+
+      EvaluationAnswer.create(
+        content: content,
+        points: 0,
+        evaluation_status: 1, 
+        evaluation_question_id: answer_data[:evaluation_question_id],
+        student_id: answer_data[:student_id]
+      )
     end
+
+    flash[:notice] = "Respuestas guardadas correctamente."
+    redirect_to course_path(course)
   end
+end
   
