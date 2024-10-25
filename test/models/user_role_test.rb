fc1f7da1f2a2695f0f242bfa11fd651a4ed41a86
cnselman <cnselman@uc.cl>
2024-10-25 13:34:58 -0300

    user role test

--- test/models/user_role_test.rb
+++ test/models/user_role_test.rb
@@ -1,133 +1,95 @@
 require "test_helper"
 
 class UserRoleTest < ActiveSupport::TestCase
+  # Reference fixtures
+  fixtures :user_roles
+
   def setup
-    @user_role = UserRole.new(
-      name: "Student",
-      description: "A student role"
-    )
+    @student_role = user_roles(:student)
+    @teacher_role = user_roles(:teacher)
+    @admin_role = user_roles(:admin)
   end
 
   test "should be valid with valid attributes" do
-    assert @user_role.valid?
-  end
-
-  test "should save successfully" do
-    assert @user_role.save
+    assert @student_role.valid?
   end
 
   test "should have many users" do
-    assert_respond_to @user_role, :users
+    assert_respond_to @student_role, :users
   end
 
   test "should be able to update attributes" do
-    @user_role.save
-    assert @user_role.update(name: "Teacher", description: "A teacher role")
-  end
-
-  test "should be able to destroy" do
-    @user_role.save
-    assert @user_role.destroy
-  end
-
-  test "should create with different roles" do
-    teacher_role = UserRole.new(name: "Teacher", description: "A teacher role")
-    admin_role = UserRole.new(name: "Admin", description: "An admin role")
-    
-    assert teacher_role.valid?
-    assert admin_role.valid?
+    assert @student_role.update(description: "Updated student role description")
   end
 
   test "should find existing role" do
-    @user_role.save
     found_role = UserRole.find_by(name: "Student")
-    assert_equal @user_role, found_role
+    assert_equal @student_role, found_role
   end
 
   test "should list all roles" do
-    @user_role.save
-    UserRole.create(name: "Teacher", description: "A teacher role")
-    assert_equal 2, UserRole.count
+    assert_equal 3, UserRole.count
   end
 
   test "should validate presence of name" do
-    @user_role.name = nil
-    assert_not @user_role.valid?
+    role = UserRole.new(description: "Test description")
+    assert_not role.valid?
   end
 
   test "should validate presence of description" do
-    @user_role.description = nil
-    assert_not @user_role.valid?
+    role = UserRole.new(name: "Test role")
+    assert_not role.valid?
   end
 
   test "should validate uniqueness of name" do
-    @user_role.save
-    duplicate_role = @user_role.dup
+    duplicate_role = UserRole.new(
+      name: "Student",
+      description: "Duplicate student role"
+    )
     assert_not duplicate_role.valid?
   end
 
   test "should handle empty description" do
-    @user_role.description = ""
-    assert_not @user_role.valid?
+    role = UserRole.new(name: "Test role", description: "")
+    assert_not role.valid?
   end
 
   test "should handle empty name" do
-    @user_role.name = ""
-    assert_not @user_role.valid?
+    role = UserRole.new(name: "", description: "Test description")
+    assert_not role.valid?
   end
 
   test "should handle role with users" do
-    @user_role.save
     user = User.create!(
       email: "test@test.com",
       password: "password",
       phone: "+56912345678",
       name: "Test User",
-      user_role: @user_role
+      user_role: @student_role
     )
-    assert_includes @user_role.users, user
+    assert_includes @student_role.users, user
   end
 
   test "should not allow role deletion with users" do
-    @user_role.save
     User.create!(
       email: "test@test.com",
       password: "password",
       phone: "+56912345678",
       name: "Test User",
-      user_role: @user_role
+      user_role: @student_role
     )
-    assert_not @user_role.destroy
-  end
-
-  test "should not save without name" do
-    @user_role.name = nil
-    assert_not @user_role.save
-  end
-
-  test "should not save without description" do
-    @user_role.description = nil
-    assert_not @user_role.save
-  end
-
-  test "should not save with duplicate name" do
-    @user_role.save
-    duplicate_role = UserRole.new(name: "Student", description: "Another student role")
-    assert_not duplicate_role.save
-  end
-
-  test "should update description only" do
-    @user_role.save
-    assert @user_role.update(description: "Updated student role description")
+    assert_not @student_role.destroy
   end
 
   test "should not update with empty name" do
-    @user_role.save
-    assert_not @user_role.update(name: "")
+    assert_not @student_role.update(name: "")
   end
 
   test "should not update with empty description" do
-    @user_role.save
-    assert_not @user_role.update(description: "")
+    assert_not @student_role.update(description: "")
+  end
+
+  test "should not update with duplicate name" do
+    assert_not @student_role.update(name: "Teacher")
   end
 end
