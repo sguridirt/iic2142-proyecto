de00dc078f19ff16603e5c9d78bf153ecaab7a0a
cnselman <cnselman@uc.cl>
2024-10-25 12:30:32 -0300

    User test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,159 +2,134 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user_role = UserRole.create!(name: 'Student', description: 'Student Role')
-    @user = User.new(
-      email: "test@example.com",
-      password: "password123",
-      password_confirmation: "password123",
-      name: "Test User",
-      phone: "+56912345678",
-      user_role: @user_role
-    )
+    @admin = users(:admin_user)
+    @teacher = users(:teacher_user)
+    @student = users(:student_user)
   end
 
-  test "should be valid with valid attributes" do
-    assert @user.valid?
+  test "valid user" do
+    assert @admin.valid?
+    assert @teacher.valid?
+    assert @student.valid?
+  end
+
+  test "should require name" do
+    user = User.new(
+      email: "test@example.com",
+      password: "password123",
+      user_role: user_roles(:admin)
+    )
+    assert_not user.valid?
+    assert_includes user.errors[:name], "can't be blank"
   end
 
   test "should require email" do
-    @user.email = nil
-    assert_not @user.valid?
-    assert_includes @user.errors[:email], "can't be blank"
+    @admin.email = nil
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:email], "can't be blank"
   end
 
   test "should require valid email format" do
-    @user.email = "invalid_email"
-    assert_not @user.valid?
-    assert_includes @user.errors[:email], "is invalid"
+    @admin.email = "invalid_email"
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:email], "is invalid"
   end
 
   test "should require unique email" do
-    duplicate_user = @user.dup
-    @user.save
+    duplicate_user = @admin.dup
     assert_not duplicate_user.valid?
+    assert_includes duplicate_user.errors[:email], "has already been taken"
   end
 
-  test "should require phone" do
-    @user.phone = nil
-    assert_not @user.valid?
-    assert_includes @user.errors[:phone], "can't be blank"
+  test "should require user_role" do
+    @admin.user_role = nil
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:user_role], "must exist"
   end
 
-  test "should require valid phone format" do
-    @user.phone = "invalid_phone"
-    assert_not @user.valid?
-    assert_includes @user.errors[:phone], "is invalid"
+  test "should have correct role associations" do
+    assert_equal "Admin", @admin.user_role.name
+    assert_equal "Teacher", @teacher.user_role.name
+    assert_equal "Student", @student.user_role.name
+  end
+
+  test "password should have minimum length" do
+    @admin.password = "12345"
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:password], "is too short (minimum is 6 characters)"
+  end
+
+  test "should accept valid profile picture" do
+    @admin.profile_picture_url = "avatar.jpg"
+    assert @admin.valid?
+  end
+
+  test "should validate phone format and presence" do
+    @admin.phone = "invalid_phone"
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:phone], "is invalid"
+    
+    @admin.phone = nil
+    assert_not @admin.valid?
+    assert_includes @admin.errors[:phone], "can't be blank"
   end
 
   test "should require unique phone" do
-    duplicate_user = @user.dup
-    @user.save
+    duplicate_user = @admin.dup
     assert_not duplicate_user.valid?
+    assert_includes duplicate_user.errors[:phone], "has already been taken"
   end
 
-  test "should belong to user_role" do
-    assert_respond_to @user, :user_role
+  test "should create associated student record" do
+    new_user = User.create(
+      name: "New Student",
+      email: "new_student@test.com",
+      password: "password123",
+      phone: "+56900000004",
+      user_role: user_roles(:student)
+    )
+    assert new_user.student.present?
   end
 
-  test "should have one student" do
-    assert_respond_to @user, :student
+  test "should create associated teacher record" do
+    new_user = User.create(
+      name: "New Teacher",
+      email: "new_teacher@test.com",
+      password: "password123",
+      phone: "+56900000005",
+      user_role: user_roles(:teacher)
+    )
+    assert new_user.teacher.present?
   end
 
-  test "should have one teacher" do
-    assert_respond_to @user, :teacher
+  test "role check methods" do
+    assert @teacher.teacher?
+    assert @student.student?
+    assert @admin.admin?
+    
+    assert_not @teacher.student?
+    assert_not @teacher.admin?
+    assert_not @student.teacher?
+    assert_not @student.admin?
+    assert_not @admin.teacher?
+    assert_not @admin.student?
   end
-
-  test "should have one admin" do
-    assert_respond_to @user, :admin
-  end
-
-  test "should have many conversation_participants" do
-    assert_respond_to @user, :conversation_participants
-  end
-
-  test "should have many conversations" do
-    assert_respond_to @user, :conversations
-  end
-
-  test "should have many messages" do
-    assert_respond_to @user, :messages
-  end
-
-  test "should create student after create when role is Student" do
-    @user.save
-    assert @user.student.present?
-  end
-
-  test "should create teacher after create when role is Teacher" do
-    @user.user_role = UserRole.create!(name: 'Teacher', description: 'Teacher Role')
-    @user.save
-    assert @user.teacher.present?
-  end
-
-  test "should be teacher?" do
-    @user.user_role = UserRole.create!(name: 'Teacher', description: 'Teacher Role')
-    assert @user.teacher?
-  end
-
-  test "should be student?" do
-    assert @user.student?
-  end
-
-  test "should be admin?" do
-    @user.user_role = UserRole.create!(name: 'Admin', description: 'Admin Role')
-    assert @user.admin?
-  end
-
+  
   test "should handle avatar attachment and thumbnail" do
-    @user.save
-    @user.avatar.attach(
+    @admin.save
+    @admin.avatar.attach(
       io: File.open(Rails.root.join('test/fixtures/files/avatar.jpg')),
       filename: 'avatar.jpg',
       content_type: 'image/jpeg'
     )
-    assert @user.avatar.attached?
-    assert_not_nil @user.avatar_thumbnail
-  end
+    assert @admin.avatar.attached?
+    assert_not_nil @admin.avatar_thumbnail
+end
 
   
 
-  test "should validate password presence" do
-    @user.password = nil
-    assert_not @user.valid?
-  end
-
-  test "should validate password confirmation" do
-    @user.password_confirmation = "different"
-    assert_not @user.valid?
-  end
-
-  test "should validate password length" do
-    @user.password = @user.password_confirmation = "short"
-    assert_not @user.valid?
-  end
-
-  test "should handle user deletion" do
-    @user.save
-    assert @user.destroy
-  end
-
-  test "should handle user update" do
-    @user.save
-    assert @user.update(name: "Updated Name")
-  end
-
-  test "should validate case insensitive email uniqueness" do
-    @user.save
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    assert_not duplicate_user.valid?
-  end
-
-  test "should validate case insensitive phone uniqueness" do
-    @user.save
-    duplicate_user = @user.dup
-    duplicate_user.phone = @user.phone.upcase
-    assert_not duplicate_user.valid?
-  end
 end
+
+
+
+
