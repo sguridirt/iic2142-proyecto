4a60729e04e8a9424cc6833e7ed6e81ad788fe01
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-10-25 10:11:20 -0300

    clean tests

--- test/controllers/teacher_reviews_controller_test.rb
+++ test/controllers/teacher_reviews_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class TeacherReviewsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get teacher_reviews_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get teacher_reviews_create_url
-    assert_response :success
-  end
 end
