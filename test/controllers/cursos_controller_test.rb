8c71aabf4a070f103e531672df2dcbcdecdea3aa
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-10-25 09:35:38 -0300

    remove test files of non existing models and controllers

--- test/controllers/cursos_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CursosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get cursos_index_url
-    assert_response :success
-  end
-end
