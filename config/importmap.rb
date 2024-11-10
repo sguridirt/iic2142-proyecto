a86b236a059da31f8229186f55b5579e54b8aebe
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-10 16:15:52 -0300

    implement datetime localization in conversations

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "evaluations", to: "evaluations.js"
+pin "conversations", to: "conversations.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
