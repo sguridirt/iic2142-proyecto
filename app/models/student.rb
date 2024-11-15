136dbd1e59551c4f2876dfc2cf1d3e3c1444c262
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 09:38:56 -0300

    add whislist model and its associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,8 @@ class Student < ApplicationRecord
   has_many :courses, through: :enrollments
   has_many :evaluation_answers, dependent: :destroy
   has_many :evaluation_feedbacks, dependent: :destroy
+  has_one :whishlist, dependent: :destroy
+  has_many :whishlisted_courses, through: :whishlist, source: :course
 
   def enrolled_in_course?(course)
     courses.include?(course)
