4636f56c498228818df647250947d5297e0b8620
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 18:20:02 -0300

    add wishlist tests

--- test/models/wishlist_test.rb
+++ test/models/wishlist_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class WishlistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @student = users(:student_user).student
+    @course1 = courses(:maths_101)
+    @course2 = courses(:maths_201)
+    @wishlist = Wishlist.create(student: @student)
+  end
+
+  test "should be valid with a student" do
+    assert @wishlist.valid?
+  end
+
+  test "should not be valid without a student" do
+    @wishlist.student = nil
+    assert_not @wishlist.valid?
+  end
+
+  test "should be able to add courses" do
+    @wishlist.courses << @course1
+    @wishlist.courses << @course2
+    assert_equal 2, @wishlist.courses.count
+  end
+
+  test "should be able to remove courses" do
+    @wishlist.courses << @course1
+    @wishlist.courses.delete(@course1)
+    assert_equal 0, @wishlist.courses.count
+  end
 end
