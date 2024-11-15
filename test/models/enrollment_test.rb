fc46cf1574a71cfd782db868aee4d0e01de9a4ac
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 19:23:57 -0300

    add more unit tests

--- test/models/enrollment_test.rb
+++ test/models/enrollment_test.rb
@@ -1,4 +1,36 @@
 require "test_helper"
 
 class EnrollmentTest < ActiveSupport::TestCase
+  def setup
+    @enrollment = enrollments(:enrollment1)
+    @student = students(:student)
+    @course = courses(:maths_101)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @enrollment.valid?
+  end
+
+  test "should not be valid without a student" do
+    @enrollment.student = nil
+    assert_not @enrollment.valid?
+  end
+
+  test "should not be valid without a course" do
+    @enrollment.course = nil
+    assert_not @enrollment.valid?
+  end
+
+  test "should belong to a student" do
+    assert_not_nil @enrollment.student
+  end
+
+  test "should belong to a course" do
+    assert_not_nil @enrollment.course
+  end
+
+  test "should not allow duplicate enrollments for the same student and course" do
+    duplicate_enrollment = @enrollment.dup
+    assert_not duplicate_enrollment.valid?
+  end
 end
--- test/models/evaluation_feedback_test.rb
+++ test/models/evaluation_feedback_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class EvaluationFeedbackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @evaluation_feedback = evaluation_feedbacks(:feedback1)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @evaluation_feedback.valid?
+  end
+
+  test "should not be valid without content" do
+    @evaluation_feedback.content = nil
+    assert_not @evaluation_feedback.valid?
+  end
+
+  test "should belong to an evaluation" do
+    assert_not_nil @evaluation_feedback.evaluation
+  end
+
+  test "should belong to a student" do
+    assert_not_nil @evaluation_feedback.student
+  end
+
+  test "should belong to a teacher" do
+    assert_not_nil @evaluation_feedback.teacher
+  end
 end
