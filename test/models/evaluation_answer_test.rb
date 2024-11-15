2b6f448265cf2f55ca49dbbf8b00e3eb82f36efe
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 18:38:16 -0300

    add unit tests

--- test/models/evaluation_answer_test.rb
+++ test/models/evaluation_answer_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class EvaluationAnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @evaluation_answer = evaluation_answers(:answer3)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @evaluation_answer.valid?
+  end
+
+  test "should not be valid without content" do
+    @evaluation_answer.content = nil
+    assert_not @evaluation_answer.valid?
+  end
+
+  test "should not be valid without points" do
+    @evaluation_answer.points = nil
+    assert_not @evaluation_answer.valid?
+  end
+
+  test "should not be valid with non-integer points" do
+    @evaluation_answer.points = 2.5
+    assert_not @evaluation_answer.valid?
+  end
+
+  test "should not be valid with negative points" do
+    @evaluation_answer.points = -1
+    assert_not @evaluation_answer.valid?
+  end
+
+  test "should belong to an evaluation question" do
+    assert_not_nil @evaluation_answer.evaluation_question
+  end
+
+  test "should belong to a student" do
+    assert_not_nil @evaluation_answer.student
+  end
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,58 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @student = students(:student)
+    @course = courses(:maths_101)
+    @wishlist = wishlists(:wishlist)
+  end
+
+  test "should be valid with a user" do
+    assert @student.valid?
+  end
+
+  test "should not be valid without a user" do
+    @student.user = nil
+    assert_not @student.valid?
+  end
+
+  test "should create a wishlist after creation" do
+    student = Student.create(user: users(:student_user))
+    assert_not_nil student.wishlist
+  end
+
+  test "should be enrolled in a course" do
+    @student.enrollments << Enrollment.create(student: @student, course: @course, enrollment_date: Date.today)
+    assert @student.enrolled_in_course?(@course)
+  end
+
+  test "should destroy associated course requests" do
+    assert_difference('CourseRequest.count', -@student.course_requests.count) do
+      @student.destroy
+    end
+  end
+
+  test "should destroy associated enrollments" do
+    assert_difference('Enrollment.count', -@student.enrollments.count) do
+      @student.destroy
+    end
+  end
+
+  test "should destroy associated evaluation answers" do
+    assert_difference('EvaluationAnswer.count', -@student.evaluation_answers.count) do
+      @student.destroy
+    end
+  end
+
+  test "should destroy associated evaluation feedbacks" do
+    assert_difference('EvaluationFeedback.count', -@student.evaluation_feedbacks.count) do
+      @student.destroy
+    end
+  end
+
+  test "should destroy associated wishlist" do
+    assert_difference('Wishlist.count', -1) do
+      @student.destroy
+    end
+  end
 end
