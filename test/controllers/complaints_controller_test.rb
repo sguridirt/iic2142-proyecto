076e39137cb37dffddbdf5809ea2095f3939bcda
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 14:58:11 -0300

    test complaints controller

--- test/controllers/complaints_controller_test.rb
+++ test/controllers/complaints_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 class ComplaintsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get new" do
-    get complaints_new_url
+    sign_in users(:student_user)
+    get new_complaint_url
     assert_response :success
   end
 
-  test "should get create" do
-    get complaints_create_url
-    assert_response :success
+  test "should create complaint" do
+    sign_in users(:student_user)
+    post complaints_url, params: { complaint: {title: "Queja", content: "banana"}}
+    assert_redirected_to home_url
+  end
+
+  test "should not create complaint with missing parameters" do
+    sign_in users(:student_user)
+    post complaints_url, params: { complaint: { title: "", content: "" } }
+    assert_response :unprocessable_entity
   end
 end
