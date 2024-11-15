a86dcaece4b95d158bd2824c95d9a75448651c4c
sguridirt <34144826+sguridirt@users.noreply.github.com>
2024-11-15 12:18:21 -0300

    implement wishlist

--- db/migrate/20241115123322_create_wishlists.rb
+++ db/migrate/20241115123322_create_wishlists.rb
@@ -2,7 +2,6 @@ class CreateWishlists < ActiveRecord::Migration[7.0]
   def change
     create_table :wishlists do |t|
       t.references :student, null: false, foreign_key: true
-      t.references :course, null: false, foreign_key: true
 
       t.timestamps
     end
