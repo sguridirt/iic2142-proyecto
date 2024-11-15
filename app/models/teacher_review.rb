4c0237e10e520132e07dd46407aebaf8263cd0b9
root <root@LAPTOP-LCS8PR3T.localdomain>
2024-11-15 09:09:00 -0300

    unit tests mumo

--- app/models/teacher_review.rb
+++ app/models/teacher_review.rb
@@ -1,4 +1,6 @@
 class TeacherReview < ApplicationRecord
   belongs_to :student 
   belongs_to :teacher 
+
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
   end
--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = admins(:admin) # Usa el fixture de admins
+  end
+
+  test "should be valid" do
+    assert @admin.valid?
+  end
+
+  test "should belong to a user" do
+    assert_not_nil @admin.user
+    assert_equal users(:admin_user), @admin.user
+  end
+
+  test "user role should be admin" do
+    assert_equal "Admin", @admin.user.user_role.name
+  end
+
+  test "should destroy associated admin if user is deleted" do
+    assert_difference "Admin.count", -1 do
+      @admin.user.destroy
+    end
+  end
 end
